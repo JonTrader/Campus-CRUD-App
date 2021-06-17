58dd1a4a4c217f8210b8042b0b739e40e6c66460
Jonathan Montoya <montoya0396@gmail.com>
2021-06-16 20:49:32 -0400

    Added boilerplater for pages

---
+++ client-side/src/Components/Campus.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class SingleCampus extends Component
+{
+    render()
+    {
+        return(
+            <></>
+        )
+    }
+}
+
+export default SingleCampus;
---
+++ client-side/src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+class navbar extends Component
+{
+    render()
+    {
+        return(
+            <></>
+        )
+    }
+}
+
+export default navbar;
